2f7e64c26236d093c6c0cd889ac46dfcf03746cc
Oscar Fernández <oscar.fernandeztrejo@gmail.com>
2025-03-05 18:37:30 -0800

    proveedor reconciliacion

--- SiExProData/Agencias.vb
+++ SiExProData/Agencias.vb
@@ -38,6 +38,7 @@ Partial Public Class Agencias
     Public Property RFC As String
     Public Property tipo_de_pago As String
     Public Property pe_remitente As Boolean
+    Public Property fedex_remitente As Boolean
 
     Public Overridable Property D_TARIFAS_AGENCIA As ICollection(Of TarifaAgencia) = New HashSet(Of TarifaAgencia)
     Public Overridable Property D_ENVIOS_PREASIGNADOS As ICollection(Of EnviosPreAsignados) = New HashSet(Of EnviosPreAsignados)
